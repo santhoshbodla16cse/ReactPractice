5b7a05e0d2a5a38e825edeebdea41569f4981eae
Bodla Santhosh <bodlasanju24@gmail.com>
2020-12-25 09:59:58 +0530

    commit

--- reactpractice/src/Components/ListRendering.js
+++ reactpractice/src/Components/ListRendering.js
@@ -4,21 +4,24 @@ import Person from './Person'
 function ListRendering() {
     const person = [
         {
+            id :1,
             name: 'santhosh',
             age: 22
         },
         {
+            id :2,
             name: 'Rohit',
             age: 21
         },
         {
+            id :3,
             name: 'umesh',
             age: 23
         }
     ]
     const personList = person.map(person => (
 
-        <Person per = {person}/>
+        <Person key ={person.id} per = {person}/>
 
         ))
     return (
